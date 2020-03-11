7a43b1b3473ae04b60a5d339eeb3e0b5dd69cde4
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2020-03-11 21:49:20 +0200

    Updates

---
+++ precache-manifest.08c7f32a0db6a64dabc7bbbc046e5ffa.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcef8d69aa8be639ed4bf0c86e16ad87",
+    "url": "/taskmanager/index.html"
+  },
+  {
+    "revision": "d4814408a93e88afdb4b",
+    "url": "/taskmanager/static/css/main.89bc8b2f.chunk.css"
+  },
+  {
+    "revision": "3087269a796bd03f9b0c",
+    "url": "/taskmanager/static/js/2.7f85bcd4.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/taskmanager/static/js/2.7f85bcd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4814408a93e88afdb4b",
+    "url": "/taskmanager/static/js/main.c89ded63.chunk.js"
+  },
+  {
+    "revision": "f0dd8c085c6df0b39a5d",
+    "url": "/taskmanager/static/js/runtime-main.1c867d2e.js"
+  },
+  {
+    "revision": "b63c56a7f6cb2581264e4129bd30ce49",
+    "url": "/taskmanager/static/media/WorkSans-Black.b63c56a7.ttf"
+  },
+  {
+    "revision": "766db6981a8f279ef14dbdc1bc8b85b6",
+    "url": "/taskmanager/static/media/WorkSans-Bold.766db698.ttf"
+  },
+  {
+    "revision": "1ed49c8ec155244026a0caf7fb577cf8",
+    "url": "/taskmanager/static/media/WorkSans-ExtraBold.1ed49c8e.ttf"
+  },
+  {
+    "revision": "43d14eda77c451857e34f336666f34a1",
+    "url": "/taskmanager/static/media/WorkSans-ExtraLight.43d14eda.ttf"
+  },
+  {
+    "revision": "121d613863a872004b04d5a1d782b3e4",
+    "url": "/taskmanager/static/media/WorkSans-Light.121d6138.ttf"
+  },
+  {
+    "revision": "eddbbc629def6aa237965521b3af404f",
+    "url": "/taskmanager/static/media/WorkSans-Medium.eddbbc62.ttf"
+  },
+  {
+    "revision": "740b8e9adcb998d4f65b3b18251fb5e2",
+    "url": "/taskmanager/static/media/WorkSans-Regular.740b8e9a.ttf"
+  },
+  {
+    "revision": "6ae86405365767c10f7e31039faab17d",
+    "url": "/taskmanager/static/media/WorkSans-SemiBold.6ae86405.ttf"
+  },
+  {
+    "revision": "cc62024e6d20a2699b38f24d89eaf5c3",
+    "url": "/taskmanager/static/media/WorkSans-Thin.cc62024e.ttf"
+  },
+  {
+    "revision": "74236ac9fd19bc821344b5a96c60a5ce",
+    "url": "/taskmanager/static/media/bg.74236ac9.jpg"
+  },
+  {
+    "revision": "2a03c37de0aa3e1a09fe9f631e661032",
+    "url": "/taskmanager/static/media/bg@2x.2a03c37d.jpg"
+  },
+  {
+    "revision": "f95c9c9f8ff4fac026f18d56fe954b41",
+    "url": "/taskmanager/static/media/ico-back.f95c9c9f.svg"
+  },
+  {
+    "revision": "be01bf45862373578c3afb9e520c9778",
+    "url": "/taskmanager/static/media/ico-logout.be01bf45.svg"
+  },
+  {
+    "revision": "a7a695a0fe0311f400c186344e3cfd7b",
+    "url": "/taskmanager/static/media/ico-reset.a7a695a0.svg"
+  },
+  {
+    "revision": "00abd7eb4b071311fea19f038f4289c4",
+    "url": "/taskmanager/static/media/ico-trash.00abd7eb.svg"
+  },
+  {
+    "revision": "6cdbf6fbddae26627eee1825b4f50a27",
+    "url": "/taskmanager/static/media/logo.6cdbf6fb.svg"
+  }
+]);
